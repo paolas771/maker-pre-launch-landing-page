69f8908c15feb3a33a3b1ce1f0f95173ff17a840
Paola Silva <paolas771@gmail.com>
2022-01-03 09:26:05 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var email = document.getElementById("emailInput");
+var error = document.getElementById("error");
+var button = document.getElementById("submit");
+button.addEventListener("click", () => {
+    if (email.value == "") {
+        email.style.border = "2px solid #FF2965";
+        error.textContent = "Oops! Please add your email";
+    } else if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value))) {
+        email.style.border = "2px solid #FF2965";
+        error.textContent = "Oops! That doesn't look like an email address";
+    }
+});
