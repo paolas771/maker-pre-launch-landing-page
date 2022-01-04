43f56010a17e0f8724c45eafb5b9b9624d940a56
Paola Silva <paolas771@gmail.com>
2022-01-04 11:49:17 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,4 +9,7 @@ button.addEventListener("click", () => {
         email.style.border = "2px solid #FF2965";
         error.textContent = "Oops! That doesn't look like an email address";
     }
+    else{
+        email.style.border = "none";
+        error.textContent = "";
 });
